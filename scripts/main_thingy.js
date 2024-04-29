aec480c210fca91c0dd062c1d7cb172ea5345711
Il-Porco-Rosso <144718841+Il-Porco-Rosso@users.noreply.github.com>
2024-04-29 17:06:21 +0000

    sdfghjk

--- scripts/main_thingy.js
+++ scripts/main_thingy.js
@@ -27,3 +27,39 @@ for (let i = 1; i <= 10; i += 1) {
     console.log(num);
 }
 console.log("done!");
+//////////////////////////////////////////////////
+console.log("task 6");
+let arr = [3, 1, 4, 1, 5];
+for (let i = 0; i <= arr.length; i += 1) {
+    console.log(arr[i]);
+}
+console.log("task 7");
+for (let i = 1; i <= arr.length; i += 1) {
+    console.log(arr.at(-i));
+}
+console.log("task 8");
+let deez = [];
+for (let i = 1; i <= 10; i += 1) {
+    deez.push(i*i*i);
+}
+
+for (let i = 0; i <= deez.length; i += 1) {
+    console.log(deez[i]);
+}
+console.log("task 9");
+let fibbisnotch = [0, 1];
+
+for (let i = 1; i <= 10; i += 1) {
+    fibbisnotch.push(fibbisnotch.at(-1) + fibbisnotch.at(-2));
+}
+for (let i = 0; i <= deez.length; i += 1) {
+    console.log(fibbisnotch[i]);
+}
+console.log("task 10");
+let nuts = [];
+for (let i = 1; i <= arr.length; i += 1) {
+    nuts.push(arr.at(-i))
+}
+for (let i = 0; i <= nuts.length; i += 1) {
+    console.log(nuts[i]);
+}
