63616b1c30f727dac1a69d92591933fa58110e98
Il-Porco-Rosso <144718841+Il-Porco-Rosso@users.noreply.github.com>
2024-05-06 17:07:24 +0000

    added 3 kingdoms

--- scripts/a_new_js_file.js
+++ scripts/a_new_js_file.js
@@ -10,3 +10,29 @@ let ZhangFeiEl = document.createElement("li");
 let HuangZhongEl = document.createElement("li");
 let MaChaoEl = document.createElement("li");
 let ZhaoYunEl = document.createElement("li");
+
+LiuBeiEl.innerText = "green";
+ZhugeLiangEl.innerText = "#BFBFBF";
+GuanYuEl.innerText = "#559F6F";
+ZhangFeiEl.innerText = "#F4556F";
+HuangZhongEl.innerText = "#CFBF6F";
+MaChaoEl.innerText = "darkolivegreen";
+ZhaoYunEl.innerText = "whitesmoke";
+
+LiuBeiEl.style.color = LiuBeiEl.innerText;
+ZhugeLiangEl.style.color = ZhugeLiangEl.innerText;
+GuanYuEl.style.color = GuanYuEl.innerText;
+ZhangFeiEl.style.color = ZhangFeiEl.innerText;
+HuangZhongEl.style.color = HuangZhongEl.innerText;
+MaChaoEl.style.color = MaChaoEl.innerText;
+ZhaoYunEl.style.color = ZhaoYunEl.innerText;
+
+ulEl.appendChild(LiuBeiEl);
+ulEl.appendChild(ZhugeLiangEl);
+ulEl.appendChild(GuanYuEl);
+ulEl.appendChild(ZhangFeiEl);
+ulEl.appendChild(HuangZhongEl);
+ulEl.appendChild(MaChaoEl);
+ulEl.appendChild(ZhaoYunEl);
+
+document.body.appendChild(ulEl);
