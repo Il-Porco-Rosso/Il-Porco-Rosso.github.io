21fbabcb933c250ba888fff3fc6872ddf8cb1ce4
Il-Porco-Rosso <144718841+Il-Porco-Rosso@users.noreply.github.com>
2024-05-06 16:46:21 +0000

    added 3 kingdoms elements

--- scripts/a_new_js_file.js
+++ scripts/a_new_js_file.js
@@ -1,2 +1,12 @@
 let shuHan = ["Liu Bei", "Zhuge Liang", "Guan Yu", "Zhang Fei", "Huang Zhong", "Ma Chao", "Zhao Yun"];
 let shuHanColors = ["green","#BFBFBF","#559F6F","#F4556F","#CFBF6F","darkolivegreen","whitesmoke"];
+
+let ulEl = document.createElement("ul");
+
+let LiuBeiEl = document.createElement("li");
+let ZhugeLiangEl = document.createElement("li");
+let GuanYuEl = document.createElement("li");
+let ZhangFeiEl = document.createElement("li");
+let HuangZhongEl = document.createElement("li");
+let MaChaoEl = document.createElement("li");
+let ZhaoYunEl = document.createElement("li");
