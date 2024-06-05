abc5e9fba8417670115364ee186d2fd458bfc700
Il-Porco-Rosso <144718841+Il-Porco-Rosso@users.noreply.github.com>
2024-06-05 21:57:47 +0000

    hvkugcj

--- scripts/waitlist.js
+++ scripts/waitlist.js
@@ -1,20 +1,27 @@
 const button = document.querySelector("#waitlistbutton");
 const popup = document.querySelector("#waitlistpopup");
+const cancel = document.querySelector("#cancelwaitlist");
+const submit = document.querySelector("#submitwaitlist");
 
 let days = 30
 
 button.addEventListener("click", addMeToTheWaitlist);
+cancel.addEventListener("click", closeTheWaitList);
+submit.addEventListener("click", submitTheWaitList);
 
 function addMeToTheWaitlist() {
-    
-popup.classList.remove("fade-out");
-    popup.classList.add("fade-in");
-
-    document.getElementById("waitlist").innerHTML = document.getElementById("waitlist").innerHTML + 
-    "<p>Name<span>" + days + "d</span></p>"
-    days += Math.floor(Math.random() * 9)
+    popup.classList.remove("fade-out");
+    popup.classList.add("fade-in");  
 }
     
-    
-//    popup.classList.remove("fade-in");
- //   popup.classList.add("fade-out");
+function closeTheWaitList() {
+    popup.classList.remove("fade-in");
+    popup.classList.add("fade-out");
+}
+
+function submitTheWaitList() {
+    document.getElementById("waitlist").innerHTML = document.getElementById("waitlist").innerHTML + 
+    "<p>Name<span>" + days + "d</span></p>"
+    days += Math.floor(Math.random() * 9);
+    closeTheWaitList();
+}
