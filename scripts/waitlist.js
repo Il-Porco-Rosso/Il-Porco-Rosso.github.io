495d3035409537779861b41fcae76f5145db2fcc
Il-Porco-Rosso <144718841+Il-Porco-Rosso@users.noreply.github.com>
2024-06-05 18:44:01 +0000

    sdfghj

--- scripts/waitlist.js
+++ scripts/waitlist.js
@@ -1,10 +1,20 @@
-const button = document.querySelector("input");
+const button = document.querySelector("#waitlistbutton");
+const popup = document.querySelector("#waitlistpopup");
 
 let days = 30
 
 button.addEventListener("click", addMeToTheWaitlist);
 
 function addMeToTheWaitlist() {
+    
+popup.classList.remove("fade-out");
+    popup.classList.add("fade-in");
+
     document.getElementById("waitlist").innerHTML = document.getElementById("waitlist").innerHTML + 
     "<p>Name<span>" + days + "d</span></p>"
-    days += Math.floor(Math.random() * 9)}
+    days += Math.floor(Math.random() * 9)
+}
+    
+    
+//    popup.classList.remove("fade-in");
+ //   popup.classList.add("fade-out");
