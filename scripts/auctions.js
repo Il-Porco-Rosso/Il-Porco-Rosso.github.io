4536f0db968771454ee16de58eb15fe414d4dc4a
Il-Porco-Rosso <144718841+Il-Porco-Rosso@users.noreply.github.com>
2024-06-07 03:24:57 +0000

    dfzhbhndhzhzhzhzh

--- scripts/auctions.js
+++ scripts/auctions.js
@@ -1,33 +1,35 @@
-//let Ibid = document.querySelector("#bid1");
-//let IIbid = document.querySelector("#bid2");
-//let IIIbid = document.querySelector("#bid3");
+let bidAmount
 let coin = 1
-
+let namelist = ["Joe Shmoe", "Jane Doe", "Jeff Beansauce", "Howard E. Snuts", "Nagisa Shiota", "Morris Merrier", "Porco Rosso", "John Wemmick"]
 setInterval(function() {
 
-    //Ibid = document.querySelector("#bid1");
-    //IIbid = document.querySelector("#bid2");
-    //IIIbid = document.querySelector("#bid3");
-
     coin = Math.round(Math.random());
+
 
     if (coin == 1) {
         coin = Math.round(Math.random());
         if (coin == 1) {
             coin = Math.round(Math.random());
             if (coin == 1) {
-                console.log("boom");
+                coin = Math.round(Math.random());
                 if (coin == 1) {
-                    console.log("boom");
+                    console.log(Math.round(Math.random()*10 - 10));
     
+                    placeNPCbid()
 
 
 
-                    
                 }
             }
         }
     }
+
+function placeNPCbid(nameId) {
+
+    document.getElementById("bid3").innerText = document.getElementById("bid2").innerText;
+    document.getElementById("bid2").innerText = document.getElementById("bid1").innerText;
+    document.getElementById("bid1").innerText = 
+}
 
   }, 500);
   
