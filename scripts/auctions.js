75cd25e419a1cb55139bb885f489fa97141f1ae5
Il-Porco-Rosso <144718841+Il-Porco-Rosso@users.noreply.github.com>
2024-06-07 04:02:57 +0000

    fixed crash bug lol

--- scripts/auctions.js
+++ scripts/auctions.js
@@ -1,34 +1,35 @@
 let bidAmount = 7000
 let decimal = 1
 let coin = 1
-let namelist = ["Jane Doe", "Aeigh Luvsekks", "Jeff Beansauce", "Howard E. Snuts", "Nagisa Shiota", "Morris Merrier", "Porco Rosso", "John Wemmick", "Karma Akabane", "Barack Obama"]
+let namelist = ["Jane Doe", "Aeigh Luvsekks", "Jeff Beansauce", "Howard E. Snuts", "Nagisa Shiota", "Morris Merrier", "Porco Rosso", "John Wemmick", "Joe Shmoe", "Barack Obama"]
 
-while (document.getElementById("hottime").style.color != "red") {
-    setInterval(function() {
 
+setInterval(function() {
+
+    if (document.getElementById("hottime").style.color != "red") {
+
+    coin = Math.round(Math.random());
+    bidAmount = bidAmount += (Math.round(Math.random() * 10));
+    decimal = Math.round(Math.random() * 100);
+    if (decimal <= 9) {
+        decimal = "0" + decimal;
+    }
+    if (decimal > 99) {
+        decimal = "00";
+    }
+    if (coin == 1) {
         coin = Math.round(Math.random());
-        bidAmount = bidAmount += (Math.round(Math.random() * 10));
-        decimal = Math.round(Math.random() * 100);
-        if (decimal <= 9) {
-            decimal = "0" + decimal;
-        }
-        if (decimal > 99) {
-            decimal = "00";
-        }
         if (coin == 1) {
             coin = Math.round(Math.random());
             if (coin == 1) {
-                coin = Math.round(Math.random());
-                if (coin == 1) {
-                    
-                        console.log("bid incoming!");
-        
-                        placeNPCbid(Math.round(Math.random()*10))
-
-                    
-                }
+    
+                    placeNPCbid(Math.round(Math.random()*10))
+                
             }
         }
+    }
+    
+}
 
     function placeNPCbid(nameId) {
 
@@ -37,8 +38,7 @@ while (document.getElementById("hottime").style.color != "red") {
         document.getElementById("bid1").innerText = namelist[nameId] + " - $" + bidAmount + "." + decimal;
     }
 
-    }, 500);
+}, 500);
     
-}
   
   
