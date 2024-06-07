51ea2d0e158291e818d7715192f97c4ea8d169cb
Il-Porco-Rosso <144718841+Il-Porco-Rosso@users.noreply.github.com>
2024-06-07 17:48:20 +0000

    added names

--- scripts/auctions.js
+++ scripts/auctions.js
@@ -1,7 +1,48 @@
-let bidAmount = 7000
-let decimal = 1
-let coin = 1
-let namelist = ["Jane Doe", "Aeigh Luvsekks", "Jeff Beansauce", "Howard E. Snuts", "Nagisa Shiota", "Morris Merrier", "Porco Rosso", "John Wemmick", "Joe Shmoe", "Barack Obama"]
+let bidAmount = 7000;
+let decimal = 1;
+let coin = 1;
+let contender = "joe";
+let namelist = [
+    "Jane Doe", 
+    "Aeigh Luvsekks", 
+    "Jeff Beansauce", 
+    "Howard E. Snuts", 
+    "Nagisa Shiota", 
+    "Morris Merrier", 
+    "Porco Rosso", 
+    "John Wemmick", 
+    "Joe Shmoe", 
+    "Barack Obama",
+    "Redstone Ned",
+    "Joe Stalin",
+    "Mike Hawk",
+    "VDV Disk",
+    "Joe Bidome",
+    "Ben Dover",
+    "Gum Guan",
+    "Guan Yu",
+    "Zhuge Liang",
+    "Dan TDM",
+    "REDREY Disk",
+    "El Donald",
+    "Shaquille O Nile",
+    "Jack from Statefarm",
+    "Kingsley Shacklebolt",
+    "Shrek",
+    "Hairy Potter",
+    "Gandalf the Grey",
+    "Jesus H. Chirst",
+    "Ghandi",
+    "Nelson Mandela",
+    "Engineer Gaming",
+    "Lucifer",
+    "Lord Hemou",
+    "Luke Skywalker",
+    "George Lucas",
+    "Dwight D. Eisenhower",
+    "J. Robert Oppenheimer",
+    "Cao Cao"
+]
 
 
 setInterval(function() {
@@ -9,7 +50,9 @@ setInterval(function() {
     if (document.getElementById("hottime").style.color != "red") {
 
     coin = Math.round(Math.random());
-    bidAmount = bidAmount += (Math.round(Math.random() * 10));
+    bidAmount += (Math.random() * 10);
+    bidAmount = Math.round(bidAmount);
+
     decimal = Math.round(Math.random() * 100);
     if (decimal <= 9) {
         decimal = "0" + decimal;
@@ -23,19 +66,22 @@ setInterval(function() {
             coin = Math.round(Math.random());
             if (coin == 1) {
     
-                    placeNPCbid(Math.round(Math.random()*10))
+                    placeNPCbid(Math.floor(Math.random()*namelist.length))
                 
             }
         }
     }
     
+} else {
+    document.getElementById("hottime").innerHTML = "SOLD to " + contender;
 }
 
     function placeNPCbid(nameId) {
 
         document.getElementById("bid3").innerText = document.getElementById("bid2").innerText;
         document.getElementById("bid2").innerText = document.getElementById("bid1").innerText;
-        document.getElementById("bid1").innerText = namelist[nameId] + " - $" + bidAmount + "." + decimal;
+        document.getElementById("bid1").innerText = namelist[nameId] + " - $" + bidAmount.toLocaleString() + "." + decimal;
+        contender = namelist[nameId];
     }
 
 }, 500);
