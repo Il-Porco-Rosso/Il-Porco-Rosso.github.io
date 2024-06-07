c985d4e9ac76ff464e69c196b64acee287382d8b
Il-Porco-Rosso <144718841+Il-Porco-Rosso@users.noreply.github.com>
2024-06-07 03:56:14 +0000

    rtyurtyurtyu

--- scripts/auctions.js
+++ scripts/auctions.js
@@ -2,41 +2,43 @@ let bidAmount = 7000
 let decimal = 1
 let coin = 1
 let namelist = ["Jane Doe", "Aeigh Luvsekks", "Jeff Beansauce", "Howard E. Snuts", "Nagisa Shiota", "Morris Merrier", "Porco Rosso", "John Wemmick", "Karma Akabane", "Barack Obama"]
-setInterval(function() {
 
-    coin = Math.round(Math.random());
-    bidAmount = bidAmount += (Math.round(Math.random() * 10));
-    decimal = Math.round(Math.random() * 100);
-    if (decimal <= 9) {
-        decimal = "0" + decimal;
-    }
-    if (decimal > 99) {
-        decimal = "00";
-    }
-    if (coin == 1) {
+while (document.getElementById("hottime").style.color != "red") {
+    setInterval(function() {
+
         coin = Math.round(Math.random());
+        bidAmount = bidAmount += (Math.round(Math.random() * 10));
+        decimal = Math.round(Math.random() * 100);
+        if (decimal <= 9) {
+            decimal = "0" + decimal;
+        }
+        if (decimal > 99) {
+            decimal = "00";
+        }
         if (coin == 1) {
             coin = Math.round(Math.random());
             if (coin == 1) {
-                
-                    console.log("bid incoming!");
-    
-                    placeNPCbid(Math.round(Math.random()*10))
+                coin = Math.round(Math.random());
+                if (coin == 1) {
+                    
+                        console.log("bid incoming!");
+        
+                        placeNPCbid(Math.round(Math.random()*10))
 
-                
+                    
+                }
             }
         }
+
+    function placeNPCbid(nameId) {
+
+        document.getElementById("bid3").innerText = document.getElementById("bid2").innerText;
+        document.getElementById("bid2").innerText = document.getElementById("bid1").innerText;
+        document.getElementById("bid1").innerText = namelist[nameId] + " - $" + bidAmount + "." + decimal;
     }
 
-function placeNPCbid(nameId) {
-
-    document.getElementById("bid3").innerText = document.getElementById("bid2").innerText;
-    document.getElementById("bid2").innerText = document.getElementById("bid1").innerText;
-    document.getElementById("bid1").innerText = namelist[nameId] + " - $" + bidAmount + "." + decimal;
+    }, 500);
+    
 }
-
-  }, 500);
-  
-  
   
   
