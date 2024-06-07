e7c308ab11be5a5f4e013732cb5e4560eed63495
Il-Porco-Rosso <144718841+Il-Porco-Rosso@users.noreply.github.com>
2024-06-07 03:51:17 +0000

    added auctions

--- scripts/auctions.js
+++ scripts/auctions.js
@@ -1,25 +1,29 @@
-let bidAmount
+let bidAmount = 7000
+let decimal = 1
 let coin = 1
-let namelist = ["Joe Shmoe", "Jane Doe", "Jeff Beansauce", "Howard E. Snuts", "Nagisa Shiota", "Morris Merrier", "Porco Rosso", "John Wemmick", "Karma Akabane"]
+let namelist = ["Jane Doe", "Aeigh Luvsekks", "Jeff Beansauce", "Howard E. Snuts", "Nagisa Shiota", "Morris Merrier", "Porco Rosso", "John Wemmick", "Karma Akabane", "Barack Obama"]
 setInterval(function() {
 
     coin = Math.round(Math.random());
-
-
+    bidAmount = bidAmount += (Math.round(Math.random() * 10));
+    decimal = Math.round(Math.random() * 100);
+    if (decimal <= 9) {
+        decimal = "0" + decimal;
+    }
+    if (decimal > 99) {
+        decimal = "00";
+    }
     if (coin == 1) {
         coin = Math.round(Math.random());
         if (coin == 1) {
             coin = Math.round(Math.random());
             if (coin == 1) {
-                coin = Math.round(Math.random());
-                if (coin == 1) {
-                    console.log(Math.round(Math.random()*10 - 10));
+                
+                    console.log("bid incoming!");
     
-                    placeNPCbid()
+                    placeNPCbid(Math.round(Math.random()*10 - 1))
 
-
-
-                }
+                
             }
         }
     }
@@ -28,7 +32,7 @@ function placeNPCbid(nameId) {
 
     document.getElementById("bid3").innerText = document.getElementById("bid2").innerText;
     document.getElementById("bid2").innerText = document.getElementById("bid1").innerText;
-    document.getElementById("bid1").innerText = namelist[nameId];
+    document.getElementById("bid1").innerText = namelist[nameId] + " - $" + bidAmount + "." + decimal;
 }
 
   }, 500);
