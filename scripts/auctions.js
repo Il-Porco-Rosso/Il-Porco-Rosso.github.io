1f54b5685fdf011dab1359fb29feddc7e820f3c4
Il-Porco-Rosso <144718841+Il-Porco-Rosso@users.noreply.github.com>
2024-06-07 03:06:51 +0000

    asafdshgsdfh

--- scripts/auctions.js
+++ scripts/auctions.js
@@ -3,7 +3,7 @@ let IIbid = document.querySelector("#bid2");
 let IIIbid = document.querySelector("#bid3");
 let coin = 1
 
-let x = setInterval(function() {
+setInterval(function() {
 
     Ibid = document.querySelector("#bid1");
     IIbid = document.querySelector("#bid2");
@@ -17,11 +17,10 @@ let x = setInterval(function() {
             coin = Math.round(Math.random());
             if (coin == 1) {
                 console.log("boom");
+
             }
         }
     }
-
-
 
   }, 500);
   
